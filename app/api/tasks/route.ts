a6538d4e4c5d88fae915ffe9250d4586dfd740ec
vasudev-io <ee20947@bristol.ac.uk>
2024-09-03 01:39:33 +0100

    task json to vercel kv db

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,27 +1,21 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import { z } from 'zod';
-import { taskSchema } from '@/data/schema';
+import { kv } from '@vercel/kv';
 import { NextRequest, NextResponse } from 'next/server';
+import { taskSchema } from '@/data/schema';
+import { z } from 'zod';
 
 export async function GET() {
-  const data = await fs.readFile(path.join(process.cwd(), '/data/tasks.json'));
-  const tasks = JSON.parse(data.toString());
+  const tasks = await kv.get('tasks') || [];
   const parsedTasks = z.array(taskSchema).parse(tasks);
   return NextResponse.json(parsedTasks);
 }
 
 export async function POST(req: NextRequest) {
   try {
-    const tasksFilePath = path.join(process.cwd(), 'data', 'tasks.json');
-    const tasksData = JSON.parse(await fs.readFile(tasksFilePath, 'utf8'));
-    
     const newTask = await req.json();
     const parsedTask = taskSchema.parse(newTask);
-    tasksData.push(parsedTask);
-    
-    await fs.writeFile(tasksFilePath, JSON.stringify(tasksData, null, 2));
-    
+    const tasks = await kv.get('tasks') || [];
+    tasks.push(parsedTask);
+    await kv.set('tasks', tasks);
     return NextResponse.json({ message: 'Task added successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error adding task:', error);
@@ -38,18 +32,9 @@ export async function DELETE(req: NextRequest) {
       return NextResponse.json({ error: 'Task ID is required' }, { status: 400 });
     }
 
-    const tasksFilePath = path.join(process.cwd(), 'data', 'tasks.json');
-    let tasksData = JSON.parse(await fs.readFile(tasksFilePath, 'utf8'));
-
-    const taskIndex = tasksData.findIndex((task: any) => task.id === id);
-
-    if (taskIndex === -1) {
-      return NextResponse.json({ error: 'Task not found' }, { status: 404 });
-    }
-
-    tasksData.splice(taskIndex, 1);
-
-    await fs.writeFile(tasksFilePath, JSON.stringify(tasksData, null, 2));
+    const tasks = await kv.get('tasks') || [];
+    const updatedTasks = tasks.filter((task: any) => task.id !== id);
+    await kv.set('tasks', updatedTasks);
 
     return NextResponse.json({ message: 'Task deleted successfully' }, { status: 200 });
   } catch (error) {
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    KV_URL: process.env.KV_URL,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
+  },
 };
 
 export default nextConfig;
